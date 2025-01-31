e376077b4618cbd9cb4849b36fbff3b1acb7d4b2
Bishru182 <bishrumohammed182@gmail.com>
2025-01-31 12:32:51 +0530

    declarirng and calling a function, Math(built in JS object)

--- src/App.js
+++ src/App.js
@@ -2,23 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const name= "dave"/* variable declaration */
+  const handleNameChange = () => {
+    const names = ['bob', 'kevin', 'dave'];
+    const int = Math.floor(Math.random() * 3);/* math.random=>produces random decimal numbers eg
+                                                eg;0.00 to 0.99999
+                                                 math.random()*3 => decimal numbers produced are multiplied by 3....therefore 0.0000to 2.999
+                                                 math.floor => above generated numbers will be round off to the nearest whole number
+                                                 SO THE OUTPUT => 0,1,2 */
+    return names[int];
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+       
         <p>
-          Edit <code>src/App.js</code> and save to see changes.
+          hello {handleNameChange()}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>{name}</p>{/* this is how expression with a value is used */}
       </header>
     </div>
   );
