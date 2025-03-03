243c84f3c8f1776fc19d068ccbe61cc898477b19
Bishru182 <bishrumohammed182@gmail.com>
2025-03-03 11:58:52 +0530

    props and prop drillin-03

--- src/Content.js
+++ src/Content.js
@@ -1,37 +1,24 @@
 import React from 'react'
 import { FaTrashAlt } from 'react-icons/fa';
+import ItemList from './ItemList';
 
-const Content = ({items,handleCheck,handleDelete}) => {
+
+const Content = ({ items,handleCheck,handleDelete }) => {
 
   return (
     <main>
       {items.length ? (
-        <ul>
-          {items.map((item) => ( 
-            <li className='item' key={item.id}>
-              <input
-                type="checkbox"
-                onChange={()=>handleCheck(item.id)}
-                checked={item.checked}
-              />
-              <label
-              style={(item.checked) ? {textDecoration: 'line-through'} : null}
-              onDoubleClick={()=>handleCheck(item.id)}>
-                {item.item}</label>
-              <FaTrashAlt 
-                  onClick={() => handleDelete(item.id)}
-                  role="button" 
-                  tabIndex="0"/* makes the trash icon focussable, so it gets activated by pressing ENTER OR SPACE  */
-              />
-
-            </li>
-
-          ))}
-        </ul>
+         <ItemList
+           items={items}
+           handleCheck={handleCheck}
+           handleDelete={handleDelete}
+         />
+        
       ) : (
         <p style={{marginTop: '2rem' }}>your list is empty</p>
 
-      ) }
+      ) 
+      }
         
     </main>
   )
