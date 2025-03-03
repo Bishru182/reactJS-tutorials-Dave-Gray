81e32942c2abf1d9ac4ca42fb07e23a4941eef37
Bishru182 <bishrumohammed182@gmail.com>
2025-03-03 10:53:58 +0530

    props and prop drilling1...>title< declared in app.js and used in header.js is an example for props

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,15 @@
-import React from 'react'
+import React from 'react'//props contain all the parent properties that have been passed to the child
 
-const Header = () => {
+const Header = ({title}) => {
  
 
   return (
     <header>
-        <h1>Grocery list</h1>
+        <h1>{title}</h1>
     </header>
   )
 }
+
+
 
 export default Header
